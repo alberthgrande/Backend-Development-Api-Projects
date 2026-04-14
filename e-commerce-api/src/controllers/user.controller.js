588fa0052621b64f0e09ded106a4b1c8a67bc73f
alberthgrande <alberthgrande98@gmail.com>
2026-04-14 12:30:52 +0800

    modified:   src/controllers/user.controller.js

--- e-commerce-api/src/controllers/user.controller.js
+++ e-commerce-api/src/controllers/user.controller.js
@@ -18,3 +18,39 @@ export const createUser = async (req, res) => {
     });
   }
 };
+
+export const updatedUser = async (req, res) => {
+  try {
+    const user = await userService.updateUser({
+      userId: req.params.id,
+      ...req.body,
+    });
+
+    res.status(201).json({
+      status: true,
+      data: user,
+      message: "User updated successfully",
+    });
+  } catch (error) {
+    res.status(400).json({
+      status: false,
+      error: error.message,
+    });
+  }
+};
+
+export const deleteUser = async (req, res) => {
+  try {
+    await userService.deleteUser(req.params.id);
+
+    res.status(201).json({
+      status: true,
+      message: "User deleted successfully",
+    });
+  } catch (error) {
+    res.status(400).json({
+      status: false,
+      error: error.message,
+    });
+  }
+};
